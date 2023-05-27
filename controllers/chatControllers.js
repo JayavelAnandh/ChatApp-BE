86e3db3ffdb1a6bad376cc5cc2c795a801adce6d
JayavelAnandh <jvanandh984@gmail.com>
2023-05-27 09:35:38 +0530

    updated

---
+++ controllers/chatControllers.js
@@ -0,0 +1,164 @@
+import Chat from "../models/chatModel.js";
+import { User } from "../models/userModel.js";
+
+export const accessChat = async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log("UserId param not sent with request");
+    return res.sendStatus(400);
+  }
+
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    var chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).json(FullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
+};
+
+export const fetchChats = async (req, res) => {
+  try {
+    let chats = await Chat.find({
+      users: { $elemMatch: { $eq: req.user.id } },
+    })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 });
+    let result = async (chats) => {
+      chats = await User.populate(chats, {
+        path: "latestMessage.sender",
+        select: "name pic email",
+      });
+      result(chats);
+    };
+    res.status(200).send(chats);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+};
+
+export const createGroup = async (req, res) => {
+  try {
+    if (!req.body.users) {
+      return res
+        .status(400)
+        .send({ message: "Please add members to the group" });
+    }
+    if (!req.body.name) {
+      return res.status(400).send({ message: "Please name the group" });
+    }
+    let members = await JSON.parse(req.body.users);
+
+    if (members.length < 2) {
+      return res
+        .status(400)
+        .send({ message: "A group needs more than 2 members" });
+    }
+    members.push(req.user);
+
+    let newGroup = await Chat.create({
+      chatName: req.body.name,
+      users: members,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    let groupDetails = await Chat.findOne({ _id: newGroup._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(groupDetails);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+};
+
+export const renameGroup = async (req, res) => {
+  try {
+    let chatId = req.body.chatId;
+    let chatName = req.body.chatName;
+
+    let renamedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      { chatName },
+      { new: true }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    res.status(200).json(renamedChat);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+};
+
+export const addMember = async (req, res) => {
+  try {
+    let chatId = req.body.chatId;
+    let userId = req.body.userId;
+
+    let newMember = await Chat.findByIdAndUpdate(
+      chatId,
+      { $push: { users: userId } },
+      { new: true }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    res.status(200).json(newMember);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+};
+export const removeMember = async (req, res) => {
+  try {
+    let chatId = req.body.chatId;
+    let userId = req.body.userId;
+
+    let newMember = await Chat.findByIdAndUpdate(
+      chatId,
+      { $pull: { users: userId } },
+      { new: true }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    res.status(200).json(newMember);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+};
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,13 +1,21 @@
 import express from "express";
+import {
+  accessChat,
+  addMember,
+  createGroup,
+  fetchChats,
+  removeMember,
+  renameGroup,
+} from "../controllers/chatControllers.js";
 import { isAuthorized } from "../middleware/authMiddleWare.js";
 
 let router = express.Router();
 
-// router.route("/").post(isAuthorized, accessChat);
-// router.route("/").get(isAuthorized, fetchChats);
-// router.route("/group").post(isAuthorized, createGroupChat);
-// router.route("/rename").put(isAuthorized, renameGroup);
-// router.route("/groupremove").put(isAuthorized, removeFromGroup);
-// router.route("/groupadd").put(isAuthorized, addToGroup);
+router.route("/").post(isAuthorized, accessChat);
+router.route("/").get(isAuthorized, fetchChats);
+router.route("/newGroup").post(isAuthorized, createGroup);
+router.route("/renameGroup").put(isAuthorized, renameGroup);
+router.route("/removeMember").put(isAuthorized, removeMember);
+router.route("/addMember").put(isAuthorized, addMember);
 
 export const chatRoutes = router;
