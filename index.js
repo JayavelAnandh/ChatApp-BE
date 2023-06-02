7e760baa8426f43294835ad0b425150da044121e
JayavelAnandh <jvanandh984@gmail.com>
2023-06-02 10:45:50 +0530

    trying to solve cors error

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ const io = new Server(server, {
   cors: {
     origin: "*",
   },
-  path: "/socket.io",
   transports: ["websocket", "polling"],
-  secure: true,
 });
 io.on("connection", (socket) => {
   console.log("connected with socket.io");
